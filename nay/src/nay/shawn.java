172464ba5c6f691e8da345cbfbad7807204a60f5
nishimay <nishiparodkar@gmail.com>
2024-02-10 08:51:21 +0530

    com

---
+++ nay/src/nay/shawn.java
@@ -0,0 +1,15 @@
+package nay;
+
+public class shawn {
+	
+	String name,rollno;
+	int age;
+	
+	shawn(String name,String rollno,int age)
+	{
+		this.name=name;
+		this.rollno=rollno;
+		this.age=age;
+	}
+
+}
